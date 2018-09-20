a71489d42cc3c82baaf419a7732ebe3ec49ef482
Devyl <42131504+Devyl-Bane@users.noreply.github.com>
2018-09-20 08:47:54 -0500

    Add files via upload

---
+++ GreetCommand.java
@@ -0,0 +1,68 @@
+package com.github.devylbane.commands.struct;
+
+import com.github.devylbane.Config;
+import com.github.devylbane.commands.struct.Command;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+public class GreetCommand extends Command
+{
+    @Override
+    public void executeCommand(@Nonnull GuildMessageReceivedEvent event, @Nonnull String invoke, @Nonnull String[] args)
+    {
+        if (event.getMessage().getMentionedMembers().isEmpty())
+        {
+            reply(event, "You need to mention a user to greet!");
+        }
+        else
+            {
+            List<User> mentionedUser = event.getMessage().getMentionedUsers();
+            reply(event, String.format("Greetings %s, how are you?", mentionedUser.get(0)));
+            }
+
+    }
+
+    @Nonnull
+    @Override
+    public String getHelp()
+    {
+        return "Greets the mentioned user!";
+    }
+
+    @Nonnull
+    @Override
+    public String getSyntax()
+    {
+        return String.format("%sgreet @user", Config.PREFIX);
+    }
+
+    @Nonnull
+    @Override
+    public String[] getAliases()
+    {
+        return new String[0];
+    }
+
+    @Nonnull
+    @Override
+    public String getName()
+    {
+        return "greet";
+    }
+
+    @Override
+    public int getRequiredArguments()
+    {
+        return 1;
+    }
+
+    @Override
+    public int getMaximumArguments()
+    {
+        return 1;
+    }
+}
